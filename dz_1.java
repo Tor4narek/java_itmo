1d088b7a8365826a2be855b239ed38ae7fe2fdee
Tor4narek <ekre4etov@yandex.ru>
2023-09-21 12:14:26 +0300

    First java hw 1.2

--- dz_1.java
+++ dz_1.java
@@ -1,10 +1,7 @@
 public class dz_1 {
     public static void main(String[] args) {
-        int funt_sum = 10, shil_sum = 3, pens_sum = 0, funt_st = 5, shil_st = 2, pens_st = 0, sum;
-        sum = (funt_sum*240+shil_sum*20+pens_sum)>=(funt_st*240+shil_st*20+pens_st)?((funt_sum*240+shil_sum*20+pens_sum)==(funt_st*240+shil_st*20+pens_st)?(0):((funt_sum*240+shil_sum*20+pens_sum) - (funt_st*240+shil_st*20+pens_st))):(-1);
-        funt_sum = sum/240;
-        shil_sum = (sum-funt_sum*240)/20;
-        pens_sum = sum-(funt_sum*240)-(shil_sum*20);
-        System.out.println(sum==0?("Сдачи нет"):(sum<0?("Ошибка"):(funt_sum+ "\n" +shil_sum+ "\n" + pens_sum)));
+        int funt_sum = 25, shil_sum = 0, pens_sum = 0, funt_st = 21, shil_st = 9, pens_st = 14, sum;
+        sum = (funt_sum*240+shil_sum*20+pens_sum) - (funt_st*240+shil_st*20+pens_st);
+        System.out.println(sum==0?("Сдачи нет"):(sum<0?("Ошибка"):(sum/240 +" фунтов"+ "\n" + (sum%240)/20 +" шилингов"+ "\n" + (sum%20)+" пенсов")));
     }
 }
